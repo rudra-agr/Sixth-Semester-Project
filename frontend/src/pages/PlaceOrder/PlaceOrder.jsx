3ea42e99efaef1138af59a0cd8728748e0d475a8
agraharirudransh <agraharirudransh01@gmail.com>
2026-02-23 20:32:21 +0545

    Update PlaceOrder component

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -28,7 +28,7 @@ const PlaceOrder = () => {
     const placeOrder = async (event) =>{
         event.preventDefault();
         let orderItems = [];
-        food_list.map((item)=>{
+        food_list.map((item) =>{
             if (cartItems[item._id]>0){
                 let itemInfo= item;
                 itemInfo["quantity"] = cartItems[item._id];
@@ -88,7 +88,7 @@ const PlaceOrder = () => {
     }
 
     const navigate = useNavigate();
-    useEffect(()=>{
+    useEffect(() =>{
         if(!token){
             navigate('/cart')
         }
